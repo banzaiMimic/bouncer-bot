efd608818b579f3b8a1ed37305f693569810b00a
drMimic <gartflores@gmail.com>
2020-09-11 16:30:16 -0400

    adding test post endpoint

---
+++ src/discord/DiscordClient.js
@@ -0,0 +1,45 @@
+const
+  Discord = require('discord.js'),
+  client = new Discord.Client(),
+  token = process.env.DISCORD_BOT_TOKEN,
+  JIRA_CHANNEL = process.env.JIRA_CHANNEL_ID
+
+const init = () => {
+  getClient()
+}
+
+const getClient = async() => {
+  return (!client.readyAt) ? client.login( token ) : client
+}
+
+client.once( 'ready', () => {
+  console.log('DiscordClient | client ready.')
+  setupListeners()
+})
+
+const setupListeners = () => {
+  console.log('  readyAt', client.readyAt)
+  client.on( 'message', async message => {
+    console.log('message', message)
+  })
+}
+
+const getChannel = async( channelId ) => {
+  let client = await getClient()
+  return client.channels.cache.get( channelId )
+}
+
+const messageChannel = async( channelId, message ) => {
+  let channel = await getChannel( channelId )
+  channel.send( message )
+}
+
+const messageJira = async( message ) => {
+  let channel = await getChannel( JIRA_CHANNEL )
+  channel.send( message )
+}
+
+module.exports = {
+  init,
+  messageJira
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ const
   bodyParser = require('body-parser'),
   app = express(),
   HOST = '0.0.0.0',
-  PORT = 8090
+  PORT = 8090,
+  DiscordClient = require('./discord/DiscordClient')
 
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded({ extended: true }))
 
 app.listen(PORT, HOST)
 
-console.log('hello ', process.env.DISCORD_TOKEN)
+DiscordClient.init()
+
+app.post('/jira/webhook', (req, res, next) => {
+
+  console.log('/jira/webhook POST ---')
+  console.log('  req.body:', req.body)
+  console.log('  req.query:', req.query)
+
+  discordApi.messageChannel( channelId, message )
+  res.send('ok')
+
+})
+
+console.log('[ihn-ai initialized]')
